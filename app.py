ecf0f34cac970869f11b65f85473b2f38f6834b2
peiwnyy <119780954+peiwnyy@users.noreply.github.com>
2022-12-11 02:49:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,40 +35,40 @@ def callback():
 
     return 'OK'
 '''
- # 訊息傳遞區塊
- # 基本上程式編輯都在這個function
- @handler.add(MessageEvent, message=TextMessage)
- def handle_message(event):
-     message = text=event.message.text
-     if re.match('告訴我秘密',message):
-         buttons_template_message = TemplateSendMessage(
-         alt_text='這個看不到',
-         template=ButtonsTemplate(
-             thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-             title='行銷搬進大程式',
-             text='選單功能－TemplateSendMessage',
-             actions=[
-                 PostbackAction(
-                     label='偷偷傳資料',
-                     display_text='檯面上',
-                     data='action=檯面下'
-                 ),
-                 MessageAction(
-                     label='光明正大傳資料',
-                     text='我就是資料'
-                 ),
-                 URIAction(
-                     label='行銷搬進大程式',
-                     uri='https://marketingliveincode.com/'
-                 )
-             ]
-         )
-     )
-         line_bot_api.reply_message(event.reply_token, buttons_template_message)
-     else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
- # 主程式
- import os
- if name == "main":
+# 訊息傳遞區塊
+# 基本上程式編輯都在這個function
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        buttons_template_message = TemplateSendMessage(
+        alt_text='這個看不到',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
+            actions=[
+                PostbackAction(
+                    label='偷偷傳資料',
+                    display_text='檯面上',
+                    data='action=檯面下'
+                ),
+                MessageAction(
+                    label='光明正大傳資料',
+                    text='我就是資料'
+                ),
+                URIAction(
+                    label='行銷搬進大程式',
+                    uri='https://marketingliveincode.com/'
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+# 主程式
+import os
+if __name__ == "__main__":
      port = int(os.environ.get('PORT', 5000))
      app.run(host='0.0.0.0', port=port)
