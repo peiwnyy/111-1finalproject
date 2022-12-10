d544794a4251d4f21e61819f0d0d74ccec0dba44
peiwnyy <119780954+peiwnyy@users.noreply.github.com>
2022-12-11 02:42:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,41 +35,40 @@ def callback():
 
     return 'OK'
 '''
-# 訊息傳遞區塊
-# 基本上程式編輯都在這個function
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+ # 訊息傳遞區塊
+ # 基本上程式編輯都在這個function
+ @handler.add(MessageEvent, message=TextMessage)
+ def handle_message(event):
      message = text=event.message.text
-     if re.match('小幫手在嗎',message):
+     if re.match('告訴我秘密',message):
          buttons_template_message = TemplateSendMessage(
          alt_text='這個看不到',
          template=ButtonsTemplate(
-             thumbnail_image_url='https://www.lib.ntu.edu.tw/img/cm_room_img1.jpg',
-             title='在！你想幹嘛呢',
-             text='有一些功能',
+             thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+             title='行銷搬進大程式',
+             text='選單功能－TemplateSendMessage',
              actions=[
-                 URIAction(
-                     label='看學餐',
-                     uri='https://meals.ntu.edu.tw/restaurant'
+                 PostbackAction(
+                     label='偷偷傳資料',
+                     display_text='檯面上',
+                     data='action=檯面下'
                  ),
                  MessageAction(
-                     label='我餓',
-                     text='我餓'
+                     label='光明正大傳資料',
+                     text='我就是資料'
                  ),
                  URIAction(
-                     label='每日卡路里',
-                     uri='https://www.hpa.gov.tw/Pages/Detail.aspx?nodeid=544&pid=726'
+                     label='行銷搬進大程式',
+                     uri='https://marketingliveincode.com/'
                  )
              ]
          )
      )
          line_bot_api.reply_message(event.reply_token, buttons_template_message)
      else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='你在叫我嗎:]'))
-
+         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
  # 主程式
-import os
-if __name__ == "__main__":
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
-
+ import os
+ if name == "main":
+     port = int(os.environ.get('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
